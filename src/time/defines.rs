89c59fa328ef630a94acef4b76ef88dec44cde10
setinne <saitinne@outlook.com>
2026-05-03 20:29:51 +0800

    Add files via upload

---
+++ src/time/defines.rs
@@ -0,0 +1,11 @@
+// Copyright (c) 2026 Setinne
+// SPDX-License-Identifier: LGPL-2.1-only
+//
+// This file is part of the TIME_MODULE project.
+// Licensed under the GNU Lesser General Public License v2.1.
+// You may obtain a copy of the License at:
+//     https://www.gnu.org/licenses/lgpl-2.1.html
+
+// 常量定义
+pub const SYNC_THRESHOLD_SECONDS: u64 = 60;
+pub const NTP_UPDATE_INTERVAL_SEC: u64 = 3600;
