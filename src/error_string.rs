e615470689a0e1dbf87984836e8c8f969098eadc
setinne <saitinne@outlook.com>
2026-05-15 21:22:18 +0800

    Clean up comments in error_string.rs
    Removed unnecessary comment lines from error_string.rs

--- src/error_string.rs
+++ src/error_string.rs
@@ -1,6 +1,5 @@
 // Copyright (c) 2026 Setinne
 // SPDX-License-Identifier: LGPL-2.1-only
-//
 // This file is part of the TIME_MODULE project.
 // Licensed under the GNU Lesser General Public License v2.1.
 // You may obtain a copy of the License at:
